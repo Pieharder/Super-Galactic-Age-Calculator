1e305df400ed4db41071d14dc9b1d63318cf1b95
Geoff Goetz <geoff.goetz503@gmail.com>
2020-02-07 11:33:57 -0800

    Add second test for Mercury

--- __test__/Galactic-Age-Calculator.test.js
+++ __test__/Galactic-Age-Calculator.test.js
@@ -10,4 +10,9 @@ describe ('PersonalAge', () => {
   test("should calculate age in years on Mercury", () => {
     expect(person.ageOnMercury()).toEqual(75)
   });
+
+  test("should calculate years left to live on Mercury assuming predefined age and lifespan", () => {
+    expect(80).toEqual(258)
+  });
+
 });
