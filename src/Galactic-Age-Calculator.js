97afd6d09423f26c21158d392b44d70b22b0d273
Geoff Goetz <geoff.goetz503@gmail.com>
2020-02-15 09:52:20 -0800

    Refactored to remove global variables and add needed semi-colons.

--- src/Galactic-Age-Calculator.js
+++ src/Galactic-Age-Calculator.js
@@ -1,84 +1,91 @@
-const mercuryDifferential = 0.24;
-const venusDifferential = 0.62;
-const marsDifferential = 1.88; 
-const jupiterDifferential = 11.86;
-
 export class PersonalAge {
 
   constructor(earthAge, earthLifeExpectency) {
-    this.earthAge = earthAge
-    this.earthLifeExpectency = earthLifeExpectency
+    this.earthAge = earthAge;
+    this.earthLifeExpectency = earthLifeExpectency;
   }
 
   ageOnMercury() {
+    const mercuryDifferential = 0.24;
     let ageInYearsMercury = (Math.floor(this.earthAge / mercuryDifferential));
     
-    return ageInYearsMercury
+    return ageInYearsMercury;
   }
 
   remainingLifeOnMercury() {
+    const mercuryDifferential = 0.24;
     let remainingYearsMercury = (Math.floor((this.earthLifeExpectency / mercuryDifferential) - (this.earthAge / mercuryDifferential)));
 
-    return remainingYearsMercury
+    return remainingYearsMercury;
   }
 
   ageOnVenus() {
+    const venusDifferential = 0.62;
     let ageInYearsVenus = (Math.floor(this.earthAge / venusDifferential));
     
-    return ageInYearsVenus
+    return ageInYearsVenus;
   }
   
   remainingLifeOnVenus() {
+    const venusDifferential = 0.62;
     let remainingYearsVenus = (Math.floor((this.earthLifeExpectency / venusDifferential) - (this.earthAge / venusDifferential)));
 
-    return remainingYearsVenus
+    return remainingYearsVenus;
   }
   
   ageOnMars() {
+    const marsDifferential = 1.88; 
     let ageInYearsMars = (Math.floor(this.earthAge / marsDifferential));
     
-    return ageInYearsMars
+    return ageInYearsMars;
   }
     
   remainingLifeOnMars() {
+    const marsDifferential = 1.88; 
     let remainingYearsMars = (Math.floor((this.earthLifeExpectency / marsDifferential) - (this.earthAge / marsDifferential)));
 
-    return remainingYearsMars
+    return remainingYearsMars;
   }
     
   ageOnJupiter() {
+    const jupiterDifferential = 11.86;
     let ageInYearsJupiter = (Math.floor(this.earthAge / jupiterDifferential));
     
-    return ageInYearsJupiter
+    return ageInYearsJupiter;
   }
       
   remainingLifeOnJupiter() {
+    const jupiterDifferential = 11.86;
     let remainingYearsJupiter = (Math.floor((this.earthLifeExpectency / jupiterDifferential) - (this.earthAge / jupiterDifferential)));
 
-    return remainingYearsJupiter
+    return remainingYearsJupiter;
   }
     
   extraLifeOnMercury() {
+    const mercuryDifferential = 0.24;
     let extraYearsMercury = (Math.floor((this.earthAge / mercuryDifferential) - (this.earthLifeExpectency / mercuryDifferential)));
 
-    return extraYearsMercury
+    return extraYearsMercury;
   }
     
   extraLifeOnVenus() {
+    const venusDifferential = 0.62;
     let extraYearsVenus = (Math.floor((this.earthAge / venusDifferential) - (this.earthLifeExpectency / venusDifferential)));
 
-    return extraYearsVenus
+    return extraYearsVenus;
   }
     
   extraLifeOnMars() {
+    const marsDifferential = 1.88; 
     let extraYearsMars = (Math.floor((this.earthAge / marsDifferential) - (this.earthLifeExpectency / marsDifferential)));
 
-    return extraYearsMars
+    return extraYearsMars;
   }
     
   extraLifeOnJupiter() {
+    const jupiterDifferential = 11.86;
     let extraYearsJupiter = (Math.floor((this.earthAge / jupiterDifferential) - (this.earthLifeExpectency / jupiterDifferential)));
 
-    return extraYearsJupiter
+    return extraYearsJupiter;
   }
 }
